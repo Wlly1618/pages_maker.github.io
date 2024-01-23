649992d6d926060f4336732b0e1a51524583da67
swilfredojoel <swilfredojoel@gmail.com>
2024-01-23 01:41:51 -0300

    making input functions, act all run

--- scripts/maker.js
+++ scripts/maker.js
@@ -22,7 +22,9 @@ const dropdown_link = document.getElementById("dropdown-link");
 const dropdown_image = document.getElementById("dropdown-image");
 const dropdown_text = document.getElementById("dropdown-text");
 
-
+/**
+ * Dropdown Functions
+ */
 var dropdownStates = {
   title: false,
   subtitle: false,
@@ -69,3 +71,93 @@ function cancelAction() {
   modal.style.display = "none";
   // Puedes realizar otras acciones de cancelación aquí si es necesario
 }
+
+/**
+ * Range Functions 
+ */
+function color_control_range(context)
+{
+  var output = document.getElementById(`span-${context}`)
+
+  var r = document.getElementById(`range-${context}-red`)
+  var g = document.getElementById(`range-${context}-green`)
+  var b = document.getElementById(`range-${context}-blue`)
+  
+  r.addEventListener("input", (f) => {
+    changeColor(r.value,g.value,b.value, output)
+  });
+  b.addEventListener("input", (f) => {
+    changeColor(r.value,g.value,b.value, output)
+  });
+  g.addEventListener("input", (f) => {
+    changeColor(r.value,g.value,b.value, output)
+  });
+}
+
+function changeColor(r, g, b, context) {
+  context.style.color = `rgb(${r},${g},${b})`;
+}
+
+function bc_color_control_range(context)
+{
+  var output = document.getElementById(`span-${context}`)
+
+  var r = document.getElementById(`range-bg-${context}-red`)
+  var g = document.getElementById(`range-bg-${context}-green`)
+  var b = document.getElementById(`range-bg-${context}-blue`)
+  
+  r.addEventListener("input", (f) => {
+    change_bg_Color(r.value,g.value,b.value, output)
+  });
+  b.addEventListener("input", (f) => {
+    change_bg_Color(r.value,g.value,b.value, output)
+  });
+  g.addEventListener("input", (f) => {
+    change_bg_Color(r.value,g.value,b.value, output)
+  });
+}
+
+function change_bg_Color(r, g, b, context) {
+  context.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
+/**
+ * Input Functions
+ */
+function load_output(context)
+{
+  document.addEventListener("DOMContentLoaded", function () {
+    var inputElement = document.getElementById(context);
+    var outputSpan = document.getElementById(`span-${context}`);
+  
+    inputElement.addEventListener("input", function () {
+        outputSpan.textContent = inputElement.value;
+    });
+  });
+}
+
+/**
+ * Font Functions
+ */
+
+function select_font(context)
+{
+  const fontSelector = document.getElementById(`fontSelector-${context}`);
+  var output = document.getElementById(`span-${context}`);
+
+  // Obtiene el valor seleccionado en el selector de fuentes
+  const selectedFont = fontSelector.value;
+
+  // Actualiza el estilo de la fuente en el span
+  output.style.fontFamily = selectedFont;
+}
+
+/**
+ * Title Calls
+ */
+load_output('title')
+color_control_range('title')
+bc_color_control_range('title')
+
+
+
